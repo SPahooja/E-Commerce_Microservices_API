5b8491054550b6e872642a4fd03362ee02b9b621
SPahooja <pahoojasahil@gmail.com>
2020-08-22 16:08:22 +0530

    Added swagger init

--- main.go
+++ main.go
@@ -1,61 +1,94 @@
 package main
 
 import (
-	"GoMicroservice/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	"github.com/go-openapi/runtime/middleware"
+
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"golang.org/x/net/context"
+	"github.com/nicholasjackson/building-microservices-youtube/product-api/data"
+	"github.com/nicholasjackson/building-microservices-youtube/product-api/handlers"
+	"github.com/nicholasjackson/env"
 )
 
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	ph := handlers.NewProduct(l)
-	//gh := handlers.Newbye(l)
+	env.Parse()
 
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+	v := data.NewValidation()
+
+	// create the handlers
+	ph := handlers.NewProducts(l, v)
+
+	// create a new serve mux and register the handlers
 	sm := mux.NewRouter()
-	getrouter := sm.Methods(http.MethodGet).Subrouter()
-	getrouter.HandleFunc("/", ph.GetProducts)
 
-	putrouter := sm.Methods(http.MethodPut).Subrouter()
-	putrouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
-	putrouter.Use(ph.MiddlewareProductValidation)
-	
-	postrouter := sm.Methods(http.MethodPost).Subrouter()
-	postrouter.HandleFunc("/", ph.AddProduct)
-	postrouter.Use(ph.MiddlewareProductValidation)
+	// handlers for API
+	getR := sm.Methods(http.MethodGet).Subrouter()
+	getR.HandleFunc("/products", ph.ListAll)
+	getR.HandleFunc("/products/{id:[0-9]+}", ph.ListSingle)
 
-	// sm.Handle("/", ph)
-	//sm.Handle("/bye", gh)
+	putR := sm.Methods(http.MethodPut).Subrouter()
+	putR.HandleFunc("/products", ph.Update)
+	putR.Use(ph.MiddlewareValidateProduct)
 
-	ser := http.Server{
-		Addr:         ":9090",
-		Handler:      sm,
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
+	postR := sm.Methods(http.MethodPost).Subrouter()
+	postR.HandleFunc("/products", ph.Create)
+	postR.Use(ph.MiddlewareValidateProduct)
+
+	deleteR := sm.Methods(http.MethodDelete).Subrouter()
+	deleteR.HandleFunc("/products/{id:[0-9]+}", ph.Delete)
+
+	// handler for documentation
+	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
+	sh := middleware.Redoc(opts, nil)
+
+	getR.Handle("/docs", sh)
+	getR.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+
+	// CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"*"}))
+
+	// create a new server
+	s := http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      ch(sm),            // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
+	// start the server
 	go func() {
-		err := ser.ListenAndServe()
-		if err != nil {
-			log.Fatal(err)
-		}
+		l.Println("Starting server on port 9090")
 
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
 	}()
 
-	sigchan := make(chan os.Signal)
-	signal.Notify(sigchan, os.Interrupt)
-	signal.Notify(sigchan, os.Kill)
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
 
-	_ = <-sigchan
-	l.Println("Received terminate, I'm leaving bro")
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
 
-	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	ser.Shutdown(tc)
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
